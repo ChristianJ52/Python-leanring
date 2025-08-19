856f48bbf20cc25365e1402eed07407f89a19b9a
ChristianJ52 <128438158+ChristianJ52@users.noreply.github.com>
2025-08-19 23:34:42 +0100

    Add files via upload

---
+++ day01_hello_world_energy_calc/hello.py
@@ -0,0 +1,11 @@
+#Energy Cost Calculator
+
+#Ask the user for an input
+energy_used = float(input("enter energy used in kwh: "))
+cost_per_kwh = float(input("eneter cost per kwh in euros: "))
+
+#Calculate total cost 
+total_cost = energy_used * cost_per_kwh
+
+#Display the result
+print(f"Total energy cost: €{total_cost.2f}")
